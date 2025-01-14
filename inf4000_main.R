fc7f8faa3cc674becd4afb23b9ca1b6c13494d3f
Grace Blazey <grblazey1@sheffield.ac.uk>
2025-01-14 14:16:00 +0000

    Initial Commit INF6027 and INF 4000 files

---
+++ inf4000_main.R
@@ -0,0 +1,158 @@
+# Libraries
+library(tidyverse)
+library(gridExtra)
+library(GGally)
+library(rgl)
+library(tidymodels)
+library(ggradar)
+library(scales)
+
+# Load in data set
+tracks<-read_csv("dataset.csv")
+
+# ~~ data cleaning
+
+# remove rows where the artist and track_name are the same
+#make column concatenating artist and track_name
+tracks$art_tra_name<-paste(tracks$artists,tracks$track_name)
+#sort by popularity (to try to avoid deleting the original version)
+tracks<-arrange(tracks,-popularity)
+#check for duplicates in this column
+tracks<-tracks[!duplicated(tracks$art_tra_name),]
+#remove the first and last columns (0 indexed one and one just made)
+tracks <- subset(tracks, select=c('track_id','artists','album_name','track_name',
+                                  'popularity','duration_ms','explicit','danceability',
+                                  'energy','key','loudness','mode','speechiness',
+                                  'acousticness','instrumentalness','liveness',
+                                  'valence','tempo','time_signature','track_genre'))
+
+heavy_metal<-tracks[grep("heavy-metal",tracks$track_genre),]
+jazz<-tracks[grep("jazz",tracks$track_genre),]
+hip_hop<-tracks[grep("hip-hop",tracks$track_genre),]
+comedy<-tracks[grep("comedy",tracks$track_genre),]
+
+new_list<-rbind(heavy_metal, jazz, hip_hop, comedy)
+
+graph_cols<-c("#D0B066A0","#048BA8A0","#16DB93A0","#A4036FA0")
+
+pca<- prcomp(new_list[,c(8,9,10,11,13,14,15,16,17,18)], scale=TRUE)
+new.pca<-data.frame(
+  track_name=new_list$track_name,
+  genre=new_list$track_genre,
+  PC1=pca$x[,1],
+  PC2=pca$x[,2]
+)
+
+# final PCA plot
+ggplot(new.pca, aes(PC1, PC2)) +
+  geom_point(aes(colour=genre, shape=genre, size=4)) +
+  theme_light() +
+  theme(text = element_text(size = 32,family = "serif")) +
+  scale_colour_manual(values=graph_cols) +
+  scale_shape_manual(values=c(15,16,17,18)) +
+  labs(title="PCA of Selected Genres", colour="Genre", shape="Genre",
+       caption="Spotify dataset", tag="1") +
+  guides(size="none")
+
+specie<-c(rep("danceability",4), rep("energy",4), rep("speechiness", 4), rep("acousticness", 4),
+          rep("instrumentalness", 4), rep("valence", 4))
+condition<-rep(c("comedy","heavy-metal","hip-hop","jazz"), 6)
+value<-c(mean(comedy$danceability), mean(heavy_metal$danceability), mean(hip_hop$danceability), mean(jazz$danceability),
+         mean(comedy$energy), mean(heavy_metal$energy), mean(hip_hop$energy), mean(jazz$energy),
+         mean(comedy$speechiness), mean(heavy_metal$speechiness), mean(hip_hop$speechiness), mean(jazz$speechiness),
+         mean(comedy$acousticness), mean(heavy_metal$acousticness), mean(hip_hop$acousticness), mean(jazz$acousticness),
+         mean(comedy$instrumentalness), mean(heavy_metal$instrumentalness), mean(hip_hop$instrumentalness), mean(jazz$instrumentalness),
+         mean(comedy$valence), mean(heavy_metal$valence), mean(hip_hop$valence), mean(jazz$valence))
+avgs<-data.frame(specie,condition,value)
+
+# final clustered bar
+ggplot(avgs, aes(fill=condition, y=value, x=specie)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  theme_light() +
+  scale_fill_manual(values=graph_cols) +
+  theme(text = element_text(size=32,family="serif"),
+        axis.text.x = element_text(size=32,angle=45, hjust=1)) +
+  labs(title="Mean of Musical Components of Selected Genres",
+       x="Musical Component", y="Mean", fill="Genre",
+       caption="Spotify dataset", tag="2")
+
+# final scatter
+ggplot(new_list, aes(valence, danceability)) +
+  geom_point(size=4, aes(colour=track_genre, shape=track_genre)) +
+  geom_smooth(method="lm", se=FALSE, colour="black") +
+  theme_light() +
+  theme(text=element_text(size=32,family="serif")) +
+  scale_colour_manual(values=graph_cols) +
+  scale_shape_manual(values=c(15,16,17,18)) +
+  labs(x="Valence", y="Danceability", colour="Genre", shape="Genre",
+       title="Danceability vs Valence for Selected Genres",
+       caption="Spotify dataset", tag="3")
+
+# final violins
+v1<-ggplot(new_list, aes(x=track_genre, y=danceability, fill=track_genre)) +
+  geom_violin() +
+  theme_light() +
+  theme(text=element_text(size=32,family="serif")) +
+  scale_fill_manual(values=graph_cols) +
+  labs(title="Violin Plot of Danceability by Genre",
+       x="Genre", y="Danceability", tag="4", caption="")+
+  guides(fill="none")
+
+v2<-ggplot(new_list, aes(x=track_genre, y=valence, fill=track_genre)) +
+  geom_violin() +
+  theme_light() +
+  theme(text=element_text(size=32,family="serif")) +
+  scale_fill_manual(values=graph_cols) +
+  labs(title="Violin Plot of Valence by Genre",
+       x="Genre", y="Valence", fill="Genre", caption="Spotify dataset", tag="")
+
+grid.arrange(v1,v2,ncol=2)
+
+# final spider
+n_new_list<-new_list
+n_new_list$popularity<-NULL
+n_new_list$duration_ms<-NULL
+n_new_list$key<-NULL
+n_new_list$loudness<-NULL
+n_new_list$mode<-NULL
+n_new_list$liveness<-NULL
+n_new_list$tempo<-NULL
+n_new_list$time_signature<-NULL
+
+n_new_list %>%
+  mutate_if(is.numeric, rescale) %>%
+  group_by(track_genre) %>%
+  summarise_if(is.numeric, mean) %>%
+  ggradar(axis.label.size=12, legend.text.size=32)
+
+# final pie
+group<-c("comedy", "heavy-metal", "hip-hop", "jazz")
+dance_values<-c(mean(comedy$danceability), mean(heavy_metal$danceability), mean(hip_hop$danceability), mean(jazz$danceability))
+pi_data<-data.frame(group,dance_values)
+
+ggplot(pi_data, aes(x="",y=dance_values,fill=group)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar(theta="y") +
+  theme(axis.line = element_blank(), panel.background = element_blank()) +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  theme(text=element_text(size=32), legend.text = element_text(size=32)) +
+  labs(x=NULL, y=NULL, fill="Genre",
+       title="Danceability proportion of songs by genre")
+
+# final parallel coords
+ggparcoord(new_list, columns=c(14,8,9,15,13,17),
+           groupColumn=20) +
+  theme(axis.text.x=element_text(angle=45),
+        panel.background=element_blank(),
+        text=element_text(size=32)) +
+  geom_vline(xintercept=c(1,2,3,4,5,6,7)) +
+  labs(colour="Genre", x="Musical Component", y="Scaled Values",
+       title="Parallel Coords of Selected Genres")
+
+
+
+
+
+
+
---
+++ inf6027_main.R
@@ -0,0 +1,205 @@
+# Libraries
+library(tidyverse)
+library(gridExtra)
+library(GGally)
+library(rgl)
+library(tidymodels)
+library(ggradar)
+library(scales)
+library(ggalluvial)
+
+# Load in data set
+tracks<-read_csv("dataset.csv")
+
+# ~~ data cleaning
+
+# remove rows where the artist and track_name are the same
+#make column concatenating artist and track_name
+tracks$art_tra_name<-paste(tracks$artists,tracks$track_name)
+#sort by popularity (to try to avoid deleting the original version)
+tracks<-arrange(tracks,-popularity)
+#check for duplicates in this column
+tracks<-tracks[!duplicated(tracks$art_tra_name),]
+#remove the first and last columns (0 indexed one and one just made)
+tracks <- subset(tracks, select=c('track_id','artists','album_name','track_name',
+                                  'popularity','duration_ms','explicit','danceability',
+                                  'energy','key','loudness','mode','speechiness',
+                                  'acousticness','instrumentalness','liveness',
+                                  'valence','tempo','time_signature','track_genre'))
+
+heavy_metal<-tracks[grep("heavy-metal",tracks$track_genre),]
+jazz<-tracks[grep("jazz",tracks$track_genre),]
+hip_hop<-tracks[grep("hip-hop",tracks$track_genre),]
+comedy<-tracks[grep("comedy",tracks$track_genre),]
+
+new_list<-rbind(heavy_metal, jazz, hip_hop, comedy)
+
+pca<- prcomp(new_list[,c(8,9,10,11,13,14,15,16,17,18)], scale=TRUE)
+new.pca<-data.frame(
+  track_name=new_list$track_name,
+  genre=new_list$track_genre,
+  PC1=pca$x[,1],
+  PC2=pca$x[,2]
+)
+
+graph_cols<-c("#D0B066A0","#048BA8A0","#16DB93A0","#A4036FA0")
+
+ggplot(new.pca, aes(PC1, PC2)) +
+  geom_point(aes(colour=genre, shape=genre, size=4)) +
+  theme_light() +
+  theme(text = element_text(size = 16,family = "serif")) +
+  scale_colour_manual(values=graph_cols) +
+  scale_shape_manual(values=c(15,16,17,18)) +
+  labs(title="PCA of Selected Genres", colour="Genre", shape="Genre",
+       caption="Spotify dataset", tag="1") +
+  guides(size="none")
+
+cor.test(comedy$popularity, comedy$acousticness)$estimate
+
+ggplot(comedy, aes(popularity, acousticness)) +
+  geom_point(colour=rgb(1,0,0,0.75)) + 
+  geom_smooth(method="lm", se=FALSE) +
+  theme_light() +
+  theme(text=element_text(size=32, family="serif")) +
+  labs(title="Popularity vs Acousticness for Comedy Genre",
+       x="Popularity", y="Acousticness", caption="Spotify dataset")
+
+
+
+# logistic regression for classification of genre
+
+# factor
+new_list$track_genre<-as.factor(new_list$track_genre)
+
+# split into training and test data
+new_list<-arrange(new_list,track_id)
+
+set.seed(421)
+split<-initial_split(new_list, prop=0.70, strata=track_genre)
+train<- split %>% training()
+test<- split %>% testing()
+
+# fit the model
+model<-multinom_reg(mode="classification", engine="nnet") %>%
+  fit(track_genre ~ danceability+energy+key+loudness+speechiness+acousticness+
+        instrumentalness+liveness+valence+tempo, data=train)
+
+tidy(model)
+
+pred_class<-predict(model, new_data=test, type="class")
+pred_proba<-predict(model, new_data=test, type="prob")
+
+# assess the model
+results<-test %>% select(track_genre) %>% bind_cols(pred_class, pred_proba)
+accuracy(results, truth=track_genre, estimate=.pred_class)
+
+# Sankey of true -> predicted genre
+results %>% group_by(track_genre, .pred_class) %>% summarise(count=n()) %>%
+  ggplot(aes(y=count, axis1=track_genre, axis2=.pred_class)) +
+  geom_alluvium(aes(fill=track_genre)) +
+  geom_stratum() +
+  geom_label(stat="stratum", aes(label=after_stat(stratum))) +
+  scale_x_discrete(limits=c("True Genre", "Predicted Genre")) +
+  theme(panel.background=element_blank(), axis.line.y=element_blank(),
+        axis.text.y=element_blank(), axis.ticks=element_blank()) +
+  labs(x="", y="") +
+  guides(fill="none")
+
+# multiple linear regression for predicting pop
+
+# randomise order of each genre df
+comedy<-comedy[sample(1:nrow(comedy)),]
+heavy_metal<-heavy_metal[sample(1:nrow(heavy_metal)),]
+hip_hop<-hip_hop[sample(1:nrow(hip_hop)),]
+jazz<-jazz[sample(1:nrow(jazz)),]
+
+# comedy model
+# split into train and test
+com_tr<-comedy[1:ceiling(nrow(comedy)*0.7),]
+com_te<-comedy[((ceiling(nrow(comedy)*0.7))+1):nrow(comedy),]
+# build model
+com_pop_mod<-lm(formula=popularity~danceability+energy+key+loudness+speechiness+
+                  acousticness+instrumentalness+liveness+valence+tempo,
+                data=com_tr)
+# get resids
+com_resid<-com_tr
+com_resid$predicted<-predict(com_pop_mod)
+com_resid$residuals<-residuals(com_pop_mod)
+
+plot(com_pop_mod, which=1)
+# make predictions
+predict(com_pop_mod, newdata=com_te, interval="confidence")
+com_pop_test<-com_te
+com_pop_test$predicted<-predict(com_pop_mod, newdata=com_te)
+com_pop_test$residuals<-com_pop_test$predicted - com_pop_test$popularity
+# get sse
+sse_com<-sum(com_pop_test$residuals**2)
+
+# heavy-metal model
+# split into train and test
+hm_tr<-heavy_metal[1:ceiling(nrow(heavy_metal)*0.7),]
+hm_te<-heavy_metal[((ceiling(nrow(heavy_metal)*0.7))+1):nrow(heavy_metal),]
+# build model
+hm_pop_mod<-lm(formula=popularity~danceability+energy+key+loudness+speechiness+
+                 acousticness+instrumentalness+liveness+valence+tempo,
+               data=hm_tr)
+# get resids
+hm_resid<-hm_tr
+hm_resid$predicted<-predict(hm_pop_mod)
+hm_resid$residuals<-residuals(hm_pop_mod)
+
+plot(hm_pop_mod, which=1)
+# make predictions
+predict(hm_pop_mod, newdata=hm_te, interval="confidence")
+hm_pop_test<-hm_te
+hm_pop_test$predicted<-predict(hm_pop_mod, newdata=hm_te)
+hm_pop_test$residuals<-hm_pop_test$predicted - hm_pop_test$popularity
+# get sse
+sse_hm<-sum(hm_pop_test$residuals**2)
+
+# hip-hop model
+# split into train and test
+hh_tr<-hip_hop[1:ceiling(nrow(hip_hop)*0.7),]
+hh_te<-hip_hop[((ceiling(nrow(hip_hop)*0.7))+1):nrow(hip_hop),]
+# build model
+hh_pop_mod<-lm(formula=popularity~danceability+energy+key+loudness+speechiness+
+                 acousticness+instrumentalness+liveness+valence+tempo,
+               data=hh_tr)
+# get resids
+hh_resid<-hh_tr
+hh_resid$predicted<-predict(hh_pop_mod)
+hh_resid$residuals<-residuals(hh_pop_mod)
+
+plot(hh_pop_mod, which=1)
+# make predictions
+predict(hh_pop_mod, newdata=hh_te, interval="confidence")
+hh_pop_test<-hh_te
+hh_pop_test$predicted<-predict(hh_pop_mod, newdata=hh_te)
+hh_pop_test$residuals<-hh_pop_test$predicted - hh_pop_test$popularity
+# get sse
+sse_hh<-sum(hh_pop_test$residuals**2)
+
+# jazz model
+# split into train and test
+jaz_tr<-jazz[1:ceiling(nrow(jazz)*0.7),]
+jaz_te<-jazz[((ceiling(nrow(jazz)*0.7))+1):nrow(jazz),]
+# build model
+jaz_pop_mod<-lm(formula=popularity~danceability+energy+key+loudness+speechiness+
+                  acousticness+instrumentalness+liveness+valence+tempo,
+                data=jaz_tr)
+# get resids
+jaz_resid<-jaz_tr
+jaz_resid$predicted<-predict(jaz_pop_mod)
+jaz_resid$residuals<-residuals(jaz_pop_mod)
+
+plot(jaz_pop_mod, which=1)
+# make predictions
+predict(jaz_pop_mod, newdata=jaz_te, interval="confidence")
+jaz_pop_test<-jaz_te
+jaz_pop_test$predicted<-predict(jaz_pop_mod, newdata=jaz_te)
+jaz_pop_test$residuals<-jaz_pop_test$predicted - jaz_pop_test$popularity
+# get sse
+sse_jaz<-sum(jaz_pop_test$residuals**2)
+
+
+
